2ff5c964e886da5cb162b3c3110b2ef7437b6368
Aristotelis <aristotelis.demetriou@gmail.com>
2023-06-06 18:43:55 +0200

    req.txt and first file

---
+++ src/data_collection/screen_capture.py
@@ -0,0 +1,13 @@
+import mss
+import mss.tools
+import time
+
+bounding_box = {'top': 100, 'left': 100, 'width': 300, 'height': 300}
+
+with mss.mss() as sct:
+    for i in range(100):
+        # screenshot = sct.grab(bounding_box)  # Take the screenshot of the primary monitor
+        screenshot = sct.grab(sct.monitors[0])
+        mss.tools.to_png(screenshot.rgb, screenshot.size, output=f'screenshot_{i}.png')  # Save to the picture file
+        time.sleep(1)
+    
